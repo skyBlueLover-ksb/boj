8d2ddad84482e70556bb96ccd42c5961f47b2811
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-03-09 23:00:50 +0900

    solve leetcode problems

--- python_algorithm_interview/chapter6/leetcode125.py
+++ python_algorithm_interview/chapter6/leetcode125.py
@@ -1,15 +1,16 @@
 import collections as cl
 
 
-def isPalindrome(self, s: str) -> bool:
-    strs: Deque = cl.deque()
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs: cl.deque = cl.deque()
 
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
 
-    while len(strs) > 1:
-        if strs.popleft() != strs.pop():
-            return False
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
 
-    return True
+        return True
---
+++ python_algorithm_interview/chapter6/leetcode819.py
@@ -0,0 +1,13 @@
+import collections
+from typing import List
+import re
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+            .lower().split()
+                 if word not in banned]
+
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]
